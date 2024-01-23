56606ab96ec836a22876236213dffad094b3bf94
Sairam Chundru <sairam.chundru1432@gmail.com>
2024-01-23 13:44:47 +0530

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, FlatList, StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
+import ToDoItem from './components/ToDoItem';
+import AddToDo from './components/AddToDo';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'buy coffe', key: '1'},
+    { text: 'create an app', key: '2'},
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  }
+
+  const submitHandler = (text) => {
+
+    if(text.length >= 3){
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prevTodos,
+        ]
+      });
+    } else {
+      Alert.alert('OOPS!', 'Note should be min 3 Chars', [
+        {text: 'OK', onPress: () => console.log('alert closed')}
+      ]);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      { /* header */}
+      <Header />
+      <View style={styles.content}>
+      { /* to form */}
+      <AddToDo submitHandler={submitHandler} />
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <ToDoItem item={item} pressHandler={pressHandler}/>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,7 +56,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  content: {
+    padding:40,
+  },
+  list: {
+    marginTop: 20,
   },
 });
